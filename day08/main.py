279c498e1371a052134f11db108095dddc53e1e3
Jachoooo <jkliskiewicz@gmail.com>
2021-12-08 14:27:55 +0100

    Part 2

--- day08/main.py
+++ day08/main.py
@@ -1,6 +1,6 @@
 import time
 
-FILENAME="input.txt"
+FILENAME="test.txt"
 if FILENAME=="test.txt":
     D=True 
 else:
@@ -14,12 +14,70 @@ inputFile=open(FILENAME,'r')
 inp=[x.strip() for x in inputFile.readlines()]
 
 #part 1
-res=0
+res1=0
 for msg in inp:
     msg=msg.split('|')[1]
     for num in msg.strip().split(' '):
         if len(num) in [2,3,4,7]:
-            res+=1
-print(res)
-           
+            res1+=1
+print("Part 1 result =",res1)
+
+
+if D:print(inp[0])
+
+ret2=0
+for msg in inp:
+
+    trueNums=['' for x in range(10)]
+    
+
+    msg=msg.replace(' | ',' ')
+    
+    for num in msg.split(' '):
+        match len(num):
+            case 2: trueNums[1]=set(num)
+            case 3: trueNums[7]=set(num)
+            case 4: trueNums[4]=set(num)
+            case 7: trueNums[8]=set(num)
+    for num in msg.split(' '):
+        if len(num) in [2,3,4,7]:
+            continue
+        if len(num)==5:
+            if len(set(num).intersection(trueNums[1]))==2:
+                trueNums[3]=set(num)
+                continue
+            if len(set(num).intersection(trueNums[4]))==2:
+                trueNums[2]=set(num)
+            if len(set(num).intersection(trueNums[4]))==3:
+                trueNums[5]=set(num)
+            continue
+        if len(num)==6:
+            if len(set(num).intersection(trueNums[1]))==2:
+                if len(set(num).intersection(trueNums[7]))==3:
+                    if len(set(num).intersection(trueNums[4]))==4:
+                        trueNums[9]=set(num)
+                    else:
+                        trueNums[0]=set(num)
+            else:
+                trueNums[6]=set(num)
+    ret2str = ''
+    
+    for j,num in enumerate(msg.split(' ')):
+        for i,trueNum in enumerate(trueNums):
+            if set(num)==trueNum:
+                ret2str+=str(i)
+                break
+        if j == 9:
+            ret2str+=' | '
+    print(ret2str)
+    ret2+=int(ret2str[-4:])
+        
+print("Part 2 result =",ret2)
+
+
+
+
+
+
+                
 
