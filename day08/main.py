21f20dd96dda08994ec36ea6cb1db3091b5d27f9
Jachoooo <jkliskiewicz@gmail.com>
2021-12-08 14:36:22 +0100

    Day 8 solution

--- day08/main.py
+++ day08/main.py
@@ -1,6 +1,6 @@
 import time
 
-FILENAME="test.txt"
+FILENAME="input.txt"
 if FILENAME=="test.txt":
     D=True 
 else:
@@ -13,6 +13,8 @@ print("\u001b[2J\u001b[0;0H")
 inputFile=open(FILENAME,'r')
 inp=[x.strip() for x in inputFile.readlines()]
 
+
+
 #part 1
 res1=0
 for msg in inp:
@@ -20,17 +22,13 @@ for msg in inp:
     for num in msg.strip().split(' '):
         if len(num) in [2,3,4,7]:
             res1+=1
-print("Part 1 result =",res1)
 
 
-if D:print(inp[0])
 
+#part 2
 ret2=0
 for msg in inp:
-
     trueNums=['' for x in range(10)]
-    
-
     msg=msg.replace(' | ',' ')
     
     for num in msg.split(' '):
@@ -39,9 +37,11 @@ for msg in inp:
             case 3: trueNums[7]=set(num)
             case 4: trueNums[4]=set(num)
             case 7: trueNums[8]=set(num)
+
     for num in msg.split(' '):
         if len(num) in [2,3,4,7]:
             continue
+
         if len(num)==5:
             if len(set(num).intersection(trueNums[1]))==2:
                 trueNums[3]=set(num)
@@ -51,6 +51,7 @@ for msg in inp:
             if len(set(num).intersection(trueNums[4]))==3:
                 trueNums[5]=set(num)
             continue
+        
         if len(num)==6:
             if len(set(num).intersection(trueNums[1]))==2:
                 if len(set(num).intersection(trueNums[7]))==3:
@@ -60,8 +61,8 @@ for msg in inp:
                         trueNums[0]=set(num)
             else:
                 trueNums[6]=set(num)
-    ret2str = ''
-    
+
+    ret2str = ''    
     for j,num in enumerate(msg.split(' ')):
         for i,trueNum in enumerate(trueNums):
             if set(num)==trueNum:
@@ -69,11 +70,13 @@ for msg in inp:
                 break
         if j == 9:
             ret2str+=' | '
-    print(ret2str)
+    
     ret2+=int(ret2str[-4:])
-        
-print("Part 2 result =",ret2)
+    print(ret2str,'|',ret2)
 
+print("\nPart 1 result =",res1)        
+print("Part 2 result =",ret2)
+print("Done in {:.6f} s".format(time.time()-starttime))
 
 
 
