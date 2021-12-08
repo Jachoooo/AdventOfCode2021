1c80390cb0f6496c00a2943a6609742b152260db
Jachoooo <jkliskiewicz@gmail.com>
2021-12-08 17:39:58 +0100

    Visualization

--- day08/main.py
+++ day08/main.py
@@ -8,8 +8,90 @@ else:
 starttime=time.time()
 
 
+def printmsg(OGmsg,NEWmsg,Nums):
+    SPC=29
+    print("\u001b[H")
+    print('[',OGmsg,end='')
+    for i in range(35-len(OGmsg.split('|')[1])):
+        print(' ',end='')
+    print(']\n[',"                                               ",
+          NEWmsg,
+          "                             ]")
+
+
+    nm=NEWmsg.split('|')[1].strip()
+    
+    chardict={}
+    chardict['E']=[x for x in Nums[8].difference(Nums[9])][0]
+    chardict['G']=[x for x in Nums[8].difference(Nums[0])][0]
+    chardict['B']=[x for x in Nums[8].difference(Nums[6])][0]
+    chardict['A']=[x for x in Nums[7].difference(Nums[1])][0]
+    chardict['D']=Nums[0].difference(Nums[7]).difference(Nums[4])
+    chardict['D'].discard(chardict['E'])
+    chardict['D']=[x for x in chardict['D']][0]
+    chardict['C']=Nums[1]
+    chardict['C'].discard(chardict['B'])
+    chardict['C']=[x for x in chardict['C']][0]
+    chardict['F']=[x for x in Nums[4].difference(Nums[3])][0]
+    
+    print('')
+    print(' '*SPC,end='')
+    for i in range(4):
+        print(' ',end='')
+        if int(nm[i]) in [0,2,3,5,6,7,8,9]:
+            print(chardict['A']*3,'    ',end='')
+        else:
+            print('        ',end='')
+    print('')
+    for j in range(2):
+        print(' '*SPC,end='')
+        for i in range(4):
+            if int(nm[i]) in [0,4,5,6,8,9]:
+                print(chardict['F'],end='')
+            else:
+                print(' ',end='')
+            print('   ',end='')
+            if int(nm[i]) in [0,1,2,3,4,7,8,9]:
+                print(chardict['B'],end='    ')
+            else:
+                print(' ',end='    ')
+        print('')
+    print(' '*SPC,end='')
+    for i in range(4):
+        print(' ',end='')
+        if int(nm[i]) in [2,3,4,5,6,8,9]:
+            print(chardict['G']*3,'    ',end='')
+        else:
+            print('        ',end='')
+    print('')
+    for j in range(2):
+        print(' '*SPC,end='')
+        for i in range(4):
+            if int(nm[i]) in [0,2,6,8]:
+                print(chardict['E'],end='')
+            else:
+                print(' ',end='')
+            print('   ',end='')
+            if int(nm[i]) in [0,1,3,4,5,6,7,8,9]:
+                print(chardict['C'],end='    ')
+            else:
+                print(' ',end='    ')
+        print('')
+    print(' '*SPC,end='')
+    for i in range(4):
+        print(' ',end='')
+        if int(nm[i]) in [0,2,3,5,6,8,9]:
+            print(chardict['D']*3,'    ',end='')
+        else:
+            print('        ',end='')
+    print('')
+    
+    time.sleep(0.01)
+    
+
 
 print("\u001b[2J\u001b[0;0H")
+print("\u001b[?25l")
 inputFile=open(FILENAME,'r')
 inp=[x.strip() for x in inputFile.readlines()]
 
@@ -29,6 +111,7 @@ for msg in inp:
 ret2=0
 for msg in inp:
     trueNums=['' for x in range(10)]
+    tempmsg=msg
     msg=msg.replace(' | ',' ')
     
     for num in msg.split(' '):
@@ -51,7 +134,7 @@ for msg in inp:
             if len(set(num).intersection(trueNums[4]))==3:
                 trueNums[5]=set(num)
             continue
-        
+
         if len(num)==6:
             if len(set(num).intersection(trueNums[1]))==2:
                 if len(set(num).intersection(trueNums[7]))==3:
@@ -72,8 +155,10 @@ for msg in inp:
             ret2str+=' | '
     
     ret2+=int(ret2str[-4:])
-    print(ret2str,'|',ret2)
+    printmsg(tempmsg,ret2str,trueNums)
+    #print(ret2str,'|',ret2)
 
+print("\u001b[?25h")
 print("\nPart 1 result =",res1)        
 print("Part 2 result =",ret2)
 print("Done in {:.6f} s".format(time.time()-starttime))
