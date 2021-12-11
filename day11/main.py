53cae2ca1f1d96843847a9a89a14925ee91ef149
Jachoooo <jkliskiewicz@gmail.com>
2021-12-11 17:53:46 +0100

    Day 11 solution

--- day11/main.py
+++ day11/main.py
@@ -1,112 +1,66 @@
-#!/usr/bin/env python3
-"""
-Advent of Code 2021
-"""
-__author__ = "Jachoooo"
-__version__ = "0.1.0"
-__license__ = "MIT"
-
-import argparse
 import time
-DAY = 0
-FPATH = __file__.rstrip(__file__.split('/')[-1])
-if FPATH == "" : FPATH = __file__.rstrip(__file__.split('\\')[-1])
+import numpy as np
+STOP=100
+FILENAME="input.txt"
+if FILENAME=="test.txt":
+    D=True 
+else:
+    D=False
+starttime=time.perf_counter()
 
 
 
-def Part1(args):
-    if args.test:
-        data=inputreader(FPATH+"test"+args.test+".txt")
-    else:
-        data=inputreader(FPATH+"input.txt")
-    
-    result=0
-    
-    if args.debug: print('[d]',data)
-    if args.verbose: print('Part_1 result = ',end='')
-    print(result)
-
-def Part2(args):
-    if args.test:
-        data=inputreader(FPATH+"test"+args.test+".txt")
-    else:
-        data=inputreader(FPATH+"input.txt")
-
-    result=0
-
-    if args.debug: print('[d]',data)
-    if args.verbose: print('Part_2 result = ',end='')
-    print(result)
-
-def inputreader(name):
-    inputFile=open(name,'r')
-    ret=[]
-    for line in inputFile:
-        ret.append(line.rstrip())
-    inputFile.close()
-    return ret
-
-def main(args):
-    """ Main entry point of the app """
-    
-    if args.verbose: print("\nAdvent of Code 2021 - Day",DAY,'\n')
-    if args.debug: print("[d]",args)
-    if args.debug: print("[d]",FPATH)
-    starttime=time.time()
-    if args.part!='2':Part1(args)
-    halftime=time.time() 
-    if args.part!='1':Part2(args)
-    if args.verbose:
+def printarray(array):
+    for y in range(1,11):
+        for x in range(1,11):
+            if array[y,x]==0 :
+                print('\u001b[1m0 \u001b[22m',end='')
+            else:
+                print(array[y,x],end=' ')
         print('')
-        if args.part==0: 
-            print("Part 1 execution time = {:.6f} s".format(halftime-starttime))
-            print("Part 2 execution time = {:.6f} s".format(time.time()-halftime))
-        print("Total execution time  = {:.6f} s".format(time.time()-starttime))
-
-if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    parser = argparse.ArgumentParser()
-
-    # Optional argument debug which defaults to False
-    parser.add_argument("-d",
-                        "--debug",
-                        action="store_true", 
-                        default=False,
-                        help="Enables debug messages")
-
-    # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("-t",
-                        "--test",
-                        action="store",
-                        default="",
-                        dest="test",
-                        help='Enables test input file')
 
 
-    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        default=False,
-        help="Enables verbose output")
 
-    # Optional part selection
-    parser.add_argument(
-        "-p",
-        "--part",
-        action="store",
-        default=0,
-        dest="part",
-        help="Select part")
+print("\u001b[2J\u001b[0;0H")
+with open(FILENAME,'r') as file:
+    array=np.genfromtxt(file,delimiter=1,dtype=np.int16)
+array=np.pad(array,1,mode='constant',constant_values=-0xfff)
 
-    # Specify output of "--version"
-    parser.add_argument(
-        "-V",
-        "--version",
-        action="version",
-        version="Version {version}".format(version=__version__))
+step=0
+flashes=0
+tempflashes=0
+res1=0
 
-    args = parser.parse_args()
-    main(args)
+while tempflashes<100 :
 
+    tempflashes=0    
+    array=array+1
+    flashtable=np.ones_like(array)
+
+    while np.sum(array>=10) != 0 :
+        Tflashtable=np.ones_like(array)
+
+        for (y,x),elem in np.ndenumerate(array):
+            if elem >= 10 and flashtable[y,x]==1:
+                Tflashtable[y,x]=0
+
+        for (y,x),F in np.ndenumerate(Tflashtable):
+            if not F and flashtable[y,x]:
+                array[y-1:y+2,x-1:x+2]+=1
+
+        flashtable &= Tflashtable
+        array *= flashtable
+                
+    flashes+=np.sum(flashtable==0)
+    tempflashes=np.sum(flashtable==0)
+
+    print("\u001b[0;0H")
+    print(f'[Step = {step+1:<7} Flashes={flashes:<7}]')
+
+    step+=1
+    printarray(array)
+    if step == 100: res1= flashes
+
+print(f"\nPart 1 result = {res1}")        
+print(f"Part 2 result = {step}")
+print("Done in {:.6f} s".format(time.perf_counter()-starttime))
