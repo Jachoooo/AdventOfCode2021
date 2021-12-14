c84938f418c9dc94f0bbe04ccfe1233e715d4fa9
Jachoooo <jkliskiewicz@gmail.com>
2021-12-14 20:28:10 +0100

    Day 14

--- day14/main.py
+++ day14/main.py
@@ -1,112 +1,92 @@
-#!/usr/bin/env python3
-"""
-Advent of Code 2021
-"""
-__author__ = "Jachoooo"
-__version__ = "0.1.0"
-__license__ = "MIT"
-
-import argparse
 import time
-DAY = 0
-FPATH = __file__.rstrip(__file__.split('/')[-1])
-if FPATH == "" : FPATH = __file__.rstrip(__file__.split('\\')[-1])
+from copy import copy
+
+FILENAME="input.txt"
+if FILENAME=="test.txt":
+    D=True 
+else:
+    D=False
+starttime=time.perf_counter()
 
 
 
-def Part1(args):
-    if args.test:
-        data=inputreader(FPATH+"test"+args.test+".txt")
-    else:
-        data=inputreader(FPATH+"input.txt")
+def expand(chars,step):
     
-    result=0
+    b=chardict[chars]    
+    numdict[b]+=1    
+    if step>1:
+        expand(chars[0]+b,step-1)
+        expand(b+chars[1],step-1)
+
+# PART 1
+chardict={}
+numdict={}
+temp=set()
+print("\u001b[2J\u001b[0;0H")
+with open(FILENAME,'r') as file:
+    startword=file.readline().strip()
+    file.readline()
+    for line in file:
+        key,val=line.strip().split(' -> ')
+        chardict[key]=val
+
+for c in startword:
+    temp.add(c)
+for c in chardict.values():
+    temp.add(c)
+for key in chardict.keys():
+    temp.add(key[0])
+    temp.add(key[1])
+
+for elem in temp:
+    numdict[elem]=0
+
+for c in startword:
+    numdict[c]+=1
+
+
+STEP=10
+for a,b in zip(startword[:-1],startword[1:]):
+    expand(a+b,STEP)
+
+res1=max(numdict.values())-min(numdict.values())
+
+#PART 2
+paircnt={}
+pairdict={}
+
+for key in chardict.keys():
+    paircnt[key]=0
+for key in chardict.keys():
+    pairdict[key]=[key[0]+chardict[key],chardict[key]+key[1]]
+
+for a,b in zip(startword[:-1],startword[1:]):
+    paircnt[a+b]+=1
+
+
+
+for key in numdict:
+    numdict[key]=0
+
+for c in startword:
+    numdict[c]+=1
+
+STEP=40
+while STEP > 0 :
+    temp=copy(paircnt)
+    for key in chardict.keys():
+        val=paircnt[key]
+        temp[pairdict[key][0]]+=val
+        temp[pairdict[key][1]]+=val
+        temp[key]-=val   
+        numdict[chardict[key]]+=val
+    paircnt=temp
+    STEP-=1
+
+res2=max(numdict.values())-min(numdict.values())
+
+
     
-    if args.debug: print('[d]',data)
-    if args.verbose: print('Part_1 result = ',end='')
-    print(result)
-
-def Part2(args):
-    if args.test:
-        data=inputreader(FPATH+"test"+args.test+".txt")
-    else:
-        data=inputreader(FPATH+"input.txt")
-
-    result=0
-
-    if args.debug: print('[d]',data)
-    if args.verbose: print('Part_2 result = ',end='')
-    print(result)
-
-def inputreader(name):
-    inputFile=open(name,'r')
-    ret=[]
-    for line in inputFile:
-        ret.append(line.rstrip())
-    inputFile.close()
-    return ret
-
-def main(args):
-    """ Main entry point of the app """
-    
-    if args.verbose: print("\nAdvent of Code 2021 - Day",DAY,'\n')
-    if args.debug: print("[d]",args)
-    if args.debug: print("[d]",FPATH)
-    starttime=time.time()
-    if args.part!='2':Part1(args)
-    halftime=time.time() 
-    if args.part!='1':Part2(args)
-    if args.verbose:
-        print('')
-        if args.part==0: 
-            print("Part 1 execution time = {:.6f} s".format(halftime-starttime))
-            print("Part 2 execution time = {:.6f} s".format(time.time()-halftime))
-        print("Total execution time  = {:.6f} s".format(time.time()-starttime))
-
-if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    parser = argparse.ArgumentParser()
-
-    # Optional argument debug which defaults to False
-    parser.add_argument("-d",
-                        "--debug",
-                        action="store_true", 
-                        default=False,
-                        help="Enables debug messages")
-
-    # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("-t",
-                        "--test",
-                        action="store",
-                        default="",
-                        dest="test",
-                        help='Enables test input file')
-
-
-    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        default=False,
-        help="Enables verbose output")
-
-    # Optional part selection
-    parser.add_argument(
-        "-p",
-        "--part",
-        action="store",
-        default=0,
-        dest="part",
-        help="Select part")
-
-    # Specify output of "--version"
-    parser.add_argument(
-        "-V",
-        "--version",
-        action="version",
-        version="Version {version}".format(version=__version__))
-
-    args = parser.parse_args()
-    main(args)
-
+print(f"Part 1 result = {res1}")     
+print(f"Part 2 result = {res2}")
+print("Done in {:.6f} s".format(time.perf_counter()-starttime))
