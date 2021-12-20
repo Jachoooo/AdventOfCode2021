05ddad64b3b19b012744a61c5430c3ad40234913
Jachoooo <jkliskiewicz@gmail.com>
2021-12-20 19:39:37 +0100

    day 20 solution

--- day20/main.py
+++ day20/main.py
@@ -1,112 +1,84 @@
-#!/usr/bin/env python3
-"""
-Advent of Code 2021
-"""
-__author__ = "Jachoooo"
-__version__ = "0.1.0"
-__license__ = "MIT"
-
-import argparse
 import time
-DAY = 0
-FPATH = __file__.rstrip(__file__.split('/')[-1])
-if FPATH == "" : FPATH = __file__.rstrip(__file__.split('\\')[-1])
+import numpy as np
+from numpy import character, product
+starttime=time.perf_counter()
+
+FILENAME="input.txt"
+D=False
+
+chardict={'#':1,'.':0}
+print("\u001b[2J\u001b[0;0H")
+with open(FILENAME,'r') as file:
+    code=file.readline()
+    _=file.readline()
+    mat=np.array([line.strip() for line in file])
 
 
-
-def Part1(args):
-    if args.test:
-        data=inputreader(FPATH+"test"+args.test+".txt")
-    else:
-        data=inputreader(FPATH+"input.txt")
+def prettyprint(mat):
+    for y in range(mat.shape[0]):
+            print('|',end='')
+            for x in range(mat.shape[1]):
+                if mat[y,x]==1:
+                    print('# ',end='')
+                else:
+                    print('  ',end='')
+            print('|')
     
-    result=0
+
+
+def expand(mat,pad):
+    global code
+    mat=np.pad(mat,2,'constant', constant_values=pad)
+    temp=np.copy(mat)
     
-    if args.debug: print('[d]',data)
-    if args.verbose: print('Part_1 result = ',end='')
-    print(result)
 
-def Part2(args):
-    if args.test:
-        data=inputreader(FPATH+"test"+args.test+".txt")
-    else:
-        data=inputreader(FPATH+"input.txt")
-
-    result=0
-
-    if args.debug: print('[d]',data)
-    if args.verbose: print('Part_2 result = ',end='')
-    print(result)
-
-def inputreader(name):
-    inputFile=open(name,'r')
-    ret=[]
-    for line in inputFile:
-        ret.append(line.rstrip())
-    inputFile.close()
-    return ret
-
-def main(args):
-    """ Main entry point of the app """
+    for y in range(mat.shape[0]-2):            
+        for x in range(mat.shape[1]-2):
+            if D:print(y,x)
+            
+            num=str(np.ravel(temp[y:y+3,x:x+3])).replace(' ','')[1:-1]
+            if D:print(num)
+            num=int(num,base=2)
+            if D:print(code[num])
+            if code[num]=='#':
+                mat[y+1,x+1]=1
+            else:
+                mat[y+1,x+1]=0
     
-    if args.verbose: print("\nAdvent of Code 2021 - Day",DAY,'\n')
-    if args.debug: print("[d]",args)
-    if args.debug: print("[d]",FPATH)
-    starttime=time.time()
-    if args.part!='2':Part1(args)
-    halftime=time.time() 
-    if args.part!='1':Part2(args)
-    if args.verbose:
-        print('')
-        if args.part==0: 
-            print("Part 1 execution time = {:.6f} s".format(halftime-starttime))
-            print("Part 2 execution time = {:.6f} s".format(time.time()-halftime))
-        print("Total execution time  = {:.6f} s".format(time.time()-starttime))
-
-if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    parser = argparse.ArgumentParser()
-
-    # Optional argument debug which defaults to False
-    parser.add_argument("-d",
-                        "--debug",
-                        action="store_true", 
-                        default=False,
-                        help="Enables debug messages")
-
-    # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("-t",
-                        "--test",
-                        action="store",
-                        default="",
-                        dest="test",
-                        help='Enables test input file')
+    mat=mat[1:-1,1:-1]
+    
+    
+    return mat,abs(pad-1)
 
 
-    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        default=False,
-        help="Enables verbose output")
 
-    # Optional part selection
-    parser.add_argument(
-        "-p",
-        "--part",
-        action="store",
-        default=0,
-        dest="part",
-        help="Select part")
 
-    # Specify output of "--version"
-    parser.add_argument(
-        "-V",
-        "--version",
-        action="version",
-        version="Version {version}".format(version=__version__))
+image=np.zeros((mat.shape[0],mat.shape[0]),dtype=np.int0)
 
-    args = parser.parse_args()
-    main(args)
+for y,elem in np.ndenumerate(mat):
+    for x,c in enumerate(elem):
+        if c == '#':
+            image[y,x]=1
+        else:
+            image[y,x]=0
+  
 
+
+#image=np.pad(image,1,'constant', constant_values=0)    
+pad=0
+for i in range(50):
+    print(2*i,'%')
+    
+    image,pad=expand(image,pad)
+
+
+print(f"\nResult = {np.sum(image)}") 
+
+print("Done in {:.6f} s".format(time.perf_counter()-starttime))
+
+#5150
+#5461 OK
+#5617
+#5696
+
+#18226
