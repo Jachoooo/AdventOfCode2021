90b1a98d76a391e2e54b961be5f83c6f8e86dadb
Jachoooo <jkliskiewicz@gmail.com>
2021-12-26 13:49:18 +0100

    day 25 solution

--- day25/main.py
+++ day25/main.py
@@ -1,112 +1,63 @@
-#!/usr/bin/env python3
-"""
-Advent of Code 2021
-"""
-__author__ = "Jachoooo"
-__version__ = "0.1.0"
-__license__ = "MIT"
-
-import argparse
 import time
-DAY = 0
-FPATH = __file__.rstrip(__file__.split('/')[-1])
-if FPATH == "" : FPATH = __file__.rstrip(__file__.split('\\')[-1])
+import numpy as np
 
 
 
-def Part1(args):
-    if args.test:
-        data=inputreader(FPATH+"test"+args.test+".txt")
-    else:
-        data=inputreader(FPATH+"input.txt")
-    
-    result=0
-    
-    if args.debug: print('[d]',data)
-    if args.verbose: print('Part_1 result = ',end='')
-    print(result)
+FILENAME="input.txt"
+if FILENAME=="test.txt":
+    D=True 
+else:
+    D=False
+starttime=time.perf_counter()
 
-def Part2(args):
-    if args.test:
-        data=inputreader(FPATH+"test"+args.test+".txt")
-    else:
-        data=inputreader(FPATH+"input.txt")
 
-    result=0
+print("\u001b[2J\u001b[0;0H")
 
-    if args.debug: print('[d]',data)
-    if args.verbose: print('Part_2 result = ',end='')
-    print(result)
+with open(FILENAME,'r') as file:
+    mat=np.genfromtxt(file,delimiter=1,dtype=str)
 
-def inputreader(name):
-    inputFile=open(name,'r')
-    ret=[]
-    for line in inputFile:
-        ret.append(line.rstrip())
-    inputFile.close()
-    return ret
 
-def main(args):
-    """ Main entry point of the app """
-    
-    if args.verbose: print("\nAdvent of Code 2021 - Day",DAY,'\n')
-    if args.debug: print("[d]",args)
-    if args.debug: print("[d]",FPATH)
-    starttime=time.time()
-    if args.part!='2':Part1(args)
-    halftime=time.time() 
-    if args.part!='1':Part2(args)
-    if args.verbose:
+def printmat(mat):
+    for y in range(mat.shape[0]):
+        for x in range(mat.shape[1]):
+            print(mat[y,x],end=' ')
         print('')
-        if args.part==0: 
-            print("Part 1 execution time = {:.6f} s".format(halftime-starttime))
-            print("Part 2 execution time = {:.6f} s".format(time.time()-halftime))
-        print("Total execution time  = {:.6f} s".format(time.time()-starttime))
 
-if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    parser = argparse.ArgumentParser()
+#if D:printmat(mat)
 
-    # Optional argument debug which defaults to False
-    parser.add_argument("-d",
-                        "--debug",
-                        action="store_true", 
-                        default=False,
-                        help="Enables debug messages")
+def advance(mat):
+    change=False
+    ret=np.copy(mat)
+    for y in range(mat.shape[0]):
+        for x in range(mat.shape[1]):
+            if mat[y,x]=='>':
+                if mat[y,(x+1)%mat.shape[1]]=='.':
+                    ret[y,x]='.'
+                    ret[y,(x+1)%mat.shape[1]]='>'
+                    change=True
 
-    # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("-t",
-                        "--test",
-                        action="store",
-                        default="",
-                        dest="test",
-                        help='Enables test input file')
+    mat=np.copy(ret)
+    
+    for x in range(mat.shape[1]):
+        for y in range(mat.shape[0]):
+            if mat[y,x]=='v':
+                if mat[(y+1)%mat.shape[0],x]=='.':
+                    ret[y,x]='.'
+                    ret[(y+1)%mat.shape[0],x]='v'
+                    change=True
+    return ret,change
 
 
-    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        default=False,
-        help="Enables verbose output")
+i=0
+change=True
+while change:  
+    i+=1          
+    mat,change=advance(mat)
+    print("\u001b[0;0H")
+    print(f'Step {i}')
+    print('')
+    if D:printmat(mat)
+    
 
-    # Optional part selection
-    parser.add_argument(
-        "-p",
-        "--part",
-        action="store",
-        default=0,
-        dest="part",
-        help="Select part")
-
-    # Specify output of "--version"
-    parser.add_argument(
-        "-V",
-        "--version",
-        action="version",
-        version="Version {version}".format(version=__version__))
-
-    args = parser.parse_args()
-    main(args)
-
+print(f"\nPart 1 result = {i}") 
+print("Done in {:.6f} s".format(time.perf_counter()-starttime))
